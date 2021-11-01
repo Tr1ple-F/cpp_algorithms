e456b4198968de169609eadf3601e03d09296c6a
Tr1ple-F <32393151+tr1ple-f@users.noreply.github.com>
2021-11-01 13:50:50 +0100

    Added merge_sort.h and quick_sort.h

--- src/sort/merge_sort.h
+++ src/sort/merge_sort.h
@@ -6,3 +6,68 @@
 #define CPP_ALGORITHMS_MERGE_SORT_H
 
 #endif //CPP_ALGORITHMS_MERGE_SORT_H
+
+namespace cpp_algorithms {
+
+    template<typename T>
+    class MergeSort : public SortingAlgo<T> {
+    public:
+        MergeSort() {}
+
+        virtual ~MergeSort() {}
+
+        void Sort(std::vector <T> &vector) override {
+            _Sort(vector, 0, vector.size() - 1);
+        }
+
+    private:
+        void merge_sort(std::vector <T> &arr, size_t low, size_t high) {
+            size_t mid;
+            if (low < high) {
+                mid = (low + high) / 2;
+                merge_sort(arr, low, mid);
+                merge_sort(arr, mid + 1, high);
+                merge(arr, low, high, mid);
+            }
+        }
+
+        void merge(std::vector <T> arr, size_t beg, size_t end, size_t mid) {
+            size_t left = mid - beg + 1;
+            size_t right = end - mid;
+
+            std::vector <T> leftArr[] = std::vector<T>(left);
+            std::vector <T> rightArr[] = std::vector<T>(right);
+
+            for (int i = 0; i < left; ++i) {
+                leftArr[i] = arr[beg + i];
+            }
+
+            for (int j = 0; j < right; ++j) {
+                rightArr[j] = arr[mid + 1 + j];
+            }
+
+            size_t i = 0, j = 0;
+            size_t k = beg;
+            while (i < left && j < right) {
+                if (!(leftArr[i] > rightArr[j])) {
+                    arr[k] = leftArr[i];
+                    i++;
+                } else {
+                    arr[k] = leftArr[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < left) {
+                arr[k] = leftArr[i];
+                i++;
+                k++;
+            }
+            while (j < right) {
+                arr[k] = rightArr[j];
+                j++;
+                k++;
+            }
+        }
+    }
+}
--- src/sort/quick_sort.h
+++ src/sort/quick_sort.h
@@ -6,3 +6,46 @@
 #define CPP_ALGORITHMS_QUICK_SORT_H
 
 #endif //CPP_ALGORITHMS_QUICK_SORT_H
+
+namespace cpp_algorithms {
+
+    template<typename T>
+    class QuickSort : public SortingAlgo<T> {
+    public:
+        QuickSort() {}
+
+        virtual ~QuickSort() {}
+
+        void Sort(std::vector <T> &vector) override {
+            _qSort(vector, 0, vector.size() - 1);
+        }
+
+    private:
+        void _qSort(std::vector <T> &A, size_t p, size_t q) {
+            int r;
+            if (p < q) {
+                r = _qPartition(A, p, q);
+                _qSort(A, p, r);
+                _qSort(A, r + 1, q);
+            }
+        }
+
+        size_t _qPartition(std::vector <T> &A, size_t p, size_t q) {
+            T x = A[p];
+            size_t i = p - 1;
+            size_t j = r + 1;
+            for (j = p + 1; j < q; j++) {
+                if (A[j] < x) {
+                    i = i + 1;
+                    T temp = A[i];
+                    A[i] = A[j];
+                    A[j] = A[i];
+                }
+            }
+            T temp = A[i];
+            A[i] = A[j];
+            A[j] = A[i];
+            return i;
+        }
+    }
+}
