ce80c27e255e6778fc4e1a599984986fdfe7d504
32393151+tr1ple-f@users.noreply.github.com <f.fehlauer@schueler.lg-ks.de>
2021-11-01 13:12:50 +0100

    Added insertion_sort.h

--- src/sort/insertion_sort.h
+++ src/sort/insertion_sort.h
@@ -6,3 +6,30 @@
 #define CPP_ALGORITHMS_INSERTION_SORT_H
 
 #endif //CPP_ALGORITHMS_INSERTION_SORT_H
+
+#include "sorting_algo.h"
+
+namespace cpp_algorithms {
+
+    template<typename T>
+    class InsertionSort : public SortingAlgo<T> {
+    public:
+        InsertionSort() {}
+
+        virtual ~InsertionSort() {}
+
+        std::vector<T> Sort(std::vector<T> &vector) override {
+            size_t n = vector.size();
+            for (int i = 1; i < n; ++i) {
+                T key = vector[i];
+                size_t j = i - 1;
+                while (j >= 0 && vector[j] > key) {
+                    vector[j + 1] = vector[j];
+                    j = j - 1;
+                }
+                vector[j + 1] = key;
+            }
+            return vector;
+        }
+    };
+}
