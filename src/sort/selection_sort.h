adcc8d339a084f97136fe41fd54cd2716b7de378
32393151+tr1ple-f@users.noreply.github.com <f.fehlauer@schueler.lg-ks.de>
2021-11-01 13:17:31 +0100

    Added selection_sort.h

--- src/sort/selection_sort.h
+++ src/sort/selection_sort.h
@@ -6,3 +6,32 @@
 #define CPP_ALGORITHMS_SELECTION_SORT_H
 
 #endif //CPP_ALGORITHMS_SELECTION_SORT_H
+
+namespace cpp_algorithms {
+
+    template<typename T>
+    class SelectionSort : public SortingAlgo<T> {
+    public:
+        SelectionSort() {}
+
+        virtual ~SelectionSort() {}
+
+        std::vector <T> Sort(std::vector <T> &vector) override {
+            size_t n = vector.size();
+            for (int i = 0; i < n - 1; ++i) {
+                size_t min = i;
+                for (int j = i + 1; j < n; ++j) {
+                    if (vector[j] < vector[min]) {
+                        min = j;
+                    }
+                }
+                if (min != i) {
+                    T temp = list[i];
+                    list[i] = list[min];
+                    list[min] = temp;
+                }
+            }
+            return vector;
+        }
+    };
+}
